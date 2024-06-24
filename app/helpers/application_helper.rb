6983f0d9de79c4d6dbca764bde37cfa152c50783
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2024-06-24 17:08:52 +0100

    chore: Add views and mailers for password change, confirmation instructions, unlock instructions, email changed, reset password instructions, and error messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def ordinal_day(day)
+    if (11..13).include?(day % 100)
+      "#{day}th"
+    else
+      case day % 10
+      when 1 then "#{day}st"
+      when 2 then "#{day}nd"
+      when 3 then "#{day}rd"
+      else "#{day}th"
+      end
+    end
+  end
+
+  def formatted_timestamp(timestamp)
+    timestamp.strftime("#{ordinal_day(timestamp.day)} %B, %Y, %H:%M %Z")
+  end
 end
